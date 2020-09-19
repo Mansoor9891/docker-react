a068e4580bbc963f5b6521fee72aff6e196cab1b
Mansoor Ahmed <mansoor9891@gmail.com>
2020-09-19 12:24:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          If this is not working,please call the Nigerian funeral guys! :(
         </p>
         <a
           className="App-link"
